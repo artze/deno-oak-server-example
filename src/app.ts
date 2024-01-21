94a8d1bd173e04ba56630c2154485c946508dd59
artze <arthurwtm@gmail.com>
2024-01-21 09:43:28 +0800

    Return 400 on ZodErrors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
-import { oak } from "@src/deps.ts";
+import { oak, z } from "@src/deps.ts";
 
 import { sumToolRouter } from "@src/sum-tool/router.ts";
 
 const app = new oak.Application();
 const router = new oak.Router();
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      ctx.response.status = 400;
+      ctx.response.body = err.issues;
+    } else {
+      throw err;
+    }
+  }
+});
 
 router.use("/sum-tool", sumToolRouter.routes());
 
